ce514fef541084db87d8f2c525e603f709bdd728
githubwswsws <wsbrianw@yahoo.com>
2015-05-13 23:30:40 -0400

    v0.8.1.2 Checkpoint Update

--- src/main.cpp
+++ src/main.cpp
@@ -4004,29 +4004,30 @@ CBlock* CreateNewBlock(CWallet* pwallet, bool fProofOfStake)
     static int64 nLastCoinStakeSearchTime = GetAdjustedTime();  // only initialized at startup
     CBlockIndex* pindexPrev = pindexBest;
 
+        
     if (fProofOfStake)  // attempt to find a coinstake
     {
-        pblock->nBits = GetNextTargetRequired(pindexPrev, true);
-        CTransaction txCoinStake;
-        int64 nSearchTime = txCoinStake.nTime; // search to current time
-        if (nSearchTime > nLastCoinStakeSearchTime)
+      pblock->nBits = GetNextTargetRequired(pindexPrev, true);
+      CTransaction txCoinStake;
+      int64 nSearchTime = txCoinStake.nTime; // search to current time
+      if ((nSearchTime - 300) > nLastCoinStakeSearchTime) // added the 300 second delay between blocks so that the CPU is not maxed out all the time.
+      {
+        // printf(">>> OK1\n");
+        if (pwallet->CreateCoinStake(*pwallet, pblock->nBits, nSearchTime - nLastCoinStakeSearchTime, txCoinStake))
         {
-			// printf(">>> OK1\n");
-            if (pwallet->CreateCoinStake(*pwallet, pblock->nBits, nSearchTime-nLastCoinStakeSearchTime, txCoinStake))
-            {
-				if (txCoinStake.nTime >= max(pindexPrev->GetMedianTimePast()+1, pindexPrev->GetBlockTime() - nMaxClockDrift))
-                {   // make sure coinstake would meet timestamp protocol
-                    // as it would be the same as the block timestamp
-                    pblock->vtx[0].vout[0].SetEmpty();
-                    pblock->vtx[0].nTime = txCoinStake.nTime;
-                    pblock->vtx.push_back(txCoinStake);
-                }
-            }
-            nLastCoinStakeSearchInterval = nSearchTime - nLastCoinStakeSearchTime;
-            nLastCoinStakeSearchTime = nSearchTime;
+          if (txCoinStake.nTime >= max(pindexPrev->GetMedianTimePast() + 1, pindexPrev->GetBlockTime() - nMaxClockDrift))
+          {   // make sure coinstake would meet timestamp protocol
+            // as it would be the same as the block timestamp
+            pblock->vtx[0].vout[0].SetEmpty();
+            pblock->vtx[0].nTime = txCoinStake.nTime;
+            pblock->vtx.push_back(txCoinStake);
+          }
         }
+        nLastCoinStakeSearchInterval = nSearchTime - nLastCoinStakeSearchTime;
+        nLastCoinStakeSearchTime = nSearchTime;
+      }
     }
-
+    
     pblock->nBits = GetNextTargetRequired(pindexPrev, pblock->IsProofOfStake());
 
     // Collect memory pool transactions into the block
